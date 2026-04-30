b625319e3a2ced7c43f7bb12943d054f5f3522f0
Naqeeb Rahmani <naqeeb.rahmani@learnet.se>
2026-04-30 15:17:27 +0200

    Fixed basic left/right movement. To continue with: the jump movement

--- Main.py
+++ Main.py
@@ -13,38 +13,52 @@ clock = pygame.time.Clock()
 game = True
 
 
-#-----------players-----------#
-
-'''class player: 
-    def __init__(self, player_sprite,x,y):
-        self.speed = 2
-        self.sprite = player_sprite
-        self.rect = pygame.Rect(x,y, 40,50)'''
-
+#-----------------------------------#
 
 pygame.init()
 
-player_1 = player(None,400,400)
+player_1 = player(None, 400, 395)
 
 
 #screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEEN_HEIGHT), pygame.FULLSCREEN)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-screen.fill((255,255,255))
+
+###################################################################################
 
 while game == True:
+
+    screen.fill((255,255,255))
 
     #level 1
 
     pygame.draw.rect(screen, (150, 75, 0), pygame.Rect(0,450, SCREEN_WIDTH + 0, SCREEN_HEIGHT-450))
     pygame.draw.rect(screen, (0, 100, 0), pygame.Rect(0,450, SCREEN_WIDTH + 0, SCREEN_HEIGHT-710))
     pygame.draw.rect(screen, (0,0,0), player_1.rect)
-    
+
     ###################################
  
+    player_1.move()
+
+    ##################################
 
     pygame.display.update()
     clock.tick(60)
     for event in pygame.event.get():
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d: player_1.direction = "Right"
+            if event.key == pygame.K_a: player_1.direction = "Left"
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d: player_1.direction = None
+            if event.key == pygame.K_a: player_1.direction = None
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            
+
+
+
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
--- Player_Class.py
+++ Player_Class.py
@@ -1,7 +1,27 @@
 import pygame
 
 class player: 
-    def __init__(self, player_sprite,x,y):
-        self.speed = 2
+    def __init__(self, player_sprite, x, y):
+        self.speed = 3
+        self.jump_height = 10
+        self.gravity = 1
+        self.velocity = 10
         self.sprite = player_sprite
-        self.rect = pygame.Rect(x,y, 40,50)
+        self.x = x; self.y = y  
+        self.rect = pygame.Rect(self.x,self.y, 50,60)
+        self.direction = None #True = Right and False = Left
+
+
+    def move(self):
+        if self.direction == "Right":
+            self.x += self.speed
+            self.rect.x = self.x
+        elif self.direction == "Left": 
+            self.x -= self.speed
+            self.rect.x = self.x
+        elif self.direction == "Up":
+            self.y -= self.velocity
+            self.velocity -= self.gravity
+            if self.velocity = 
+
+        
