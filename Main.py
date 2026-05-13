37786cfecddba4c9cefd187393be0d581755fc52
Naqeeb Rahmani <naqeeb.rahmani@learnet.se>
2026-05-13 20:45:38 +0200

    almost done with the map

--- Main.py
+++ Main.py
@@ -18,8 +18,9 @@ clock = pygame.time.Clock()
 
 #---------game-variables------------#
 
-game = True
+game= True
 
+mode = "game"
 
 #-----------------------------------#
 
@@ -132,8 +133,15 @@ display_number_of_coins = display_number_of_coins_font.render(f"Coin: {player_2.
 
 ####map####
 
-floor = platform(MAP_WIDTH, 270, 0, 450, (150,75,0))
-grass = platform(MAP_WIDTH, SCREEN_HEIGHT - 710, 0, 450, (0,100,0))
+### background ###
+
+background = pygame.image.load("Assets\Map\Background\Dungeon_brick_wall_grey.png")
+
+##################
+
+
+floor = platform(MAP_WIDTH + 2000, 270, 0, 450, (150,75,0))
+grass = platform(MAP_WIDTH + 2000, SCREEN_HEIGHT - 710, 0, 450, (0,100,0))
 roof = platform(MAP_WIDTH, 5, 0,0, (0,0,0))
 
 wall_left = platform(20, SCREEN_HEIGHT + 100, 0, -100, (10,10,10))
@@ -173,28 +181,93 @@ lever_3 = lever(780, 65, big_lever_off_sprite, big_lever_on_sprite,"lever_3", Fa
 platform_6 = platform(100, 20, 870, 200, (0,0,0))
 platform_6_extension = platform(100, 20, 870, 200, (0,0,0))
 
+plate_2 = pressure_plate(900,440)
 
+coin_3 = coin(900, 220, pygame.transform.scale(coin_spritesheet.subsurface(0,0,16,16), (50,50)))
 
+wall_4 = platform(20, 250, 970, 200, (0,0,0))
 
-################
+lever_4 = lever(1100, 385, big_lever_off_sprite, big_lever_on_sprite,"lever_4", False)
+
+platform_7 = platform(200, 20, 1200, 300, (0,0,0))
+
+platform_8 = platform(200, 20, 1200, 100, (0,0,0))
+
+lever_5 = lever(1220, 45, small_lever_off_sprite, small_lever_on_sprite,"lever_5", True)
+
+lever_6 = lever(1330, 45, small_lever_off_sprite, small_lever_on_sprite,"lever_6", True)
+
+coin_4 = coin(1270, 250, pygame.transform.scale(coin_spritesheet.subsurface(0,0,16,16), (50,50)))
+
+platform_9 = platform(250, 20, 1700, 200, (0,0,0))
+
+plate_3 = pressure_plate(1800, 190)
+
+coin_5 = coin(1800, 220, pygame.transform.scale(coin_spritesheet.subsurface(0,0,16,16), (50,50)))
+
+coin_6 = coin(1800, 20, pygame.transform.scale(coin_spritesheet.subsurface(0,0,16,16), (50,50)))
+
+wall_5_upper_part = platform(20, 200, 1950, 0, (0,0,0))
+wall_5_under_part = platform(20, 250, 1950, 200, (0,0,0))
+
+###################
+plate_4 = pressure_plate(2050, 440)
+
+wall_6 = platform(20, 450, 3000, 0, (0,0,0))
+
+platform_10 = platform(200, 20, 1950, 200, (0,0,0))
+
+lever_7 = lever(2000, 150, small_lever_off_sprite, small_lever_on_sprite,"lever_7", True)
+
+platform_11 = platform(150, 20, 2400, 300, (0,0,0))
+
+plate_5 = pressure_plate(2455, 290)
+
+coin_7 = coin(2450, 100, pygame.transform.scale(coin_spritesheet.subsurface(0,0,16,16), (50,50)))
+
+coin_8 = coin(2900, 50, pygame.transform.scale(coin_spritesheet.subsurface(0,0,16,16), (50,50)))
+
+lever_8 = lever(2035, 145, small_lever_off_sprite, small_lever_on_sprite,"lever_8", True)
+
+lever_9 = lever(2450, 385, big_lever_off_sprite, big_lever_on_sprite,"lever_9", False)
+
+wall_7 = platform(20, 100, 2850, 0, (0,0,0))
+
+platform_12 = platform(150, 20, 2850, 100, (0,0,0))
+
+lever_10 = lever(2850, 385, small_lever_off_sprite, small_lever_on_sprite,"lever_10", True)
+
+###############33
 
 
 platforms = [floor.rect, grass.rect, roof.rect ,wall_left.rect, platform_1.rect, platform_2.rect, platform_2_plate.rect ,wall_under_platform_2.rect, wall_above_platform_2.rect,
-wall_3_below.rect, wall_3_above.rect,platform_3.rect, platform_4.rect, platform_5.rect, platform_6.rect, platform_6_extension.rect]
+wall_3_below.rect, wall_3_above.rect,platform_3.rect, platform_4.rect, platform_5.rect, platform_6.rect, platform_6_extension.rect, plate_2.rect, wall_4.rect, platform_7.rect, platform_8.rect, platform_9.rect, 
+wall_5_upper_part.rect, wall_5_under_part.rect, plate_3.rect, wall_6.rect, plate_4.rect, platform_10.rect, platform_11.rect, plate_5.rect,wall_7.rect, platform_12.rect]
 
-horizontally_moving_platforms = [platform_6_extension]
+horizontally_moving_platforms = [platform_6_extension, platform_12]
 
-pressure_plates = [platform_2_plate]
+pressure_plates = [platform_2_plate, plate_2, plate_3, plate_4, plate_5]
 
-non_collideable_objects = [lever_1, coin_1, lever_2, coin_2, lever_3]
+non_collideable_objects = [lever_1, coin_1, lever_2, coin_2, lever_3, coin_3, lever_4, lever_5, lever_6, coin_4, coin_5, coin_6, coin_7, coin_8, lever_8, lever_9]
 
-levers = [lever_1, lever_2, lever_3]
-coins = [coin_1, coin_2]
+levers = [lever_1, lever_2, lever_3, lever_4, lever_5, lever_6, lever_7, lever_8, lever_9, lever_10]
+coins = [coin_1, coin_2, coin_3, coin_4, coin_5, coin_6, coin_7, coin_8]
 
 for coin in coins:
     coin.get_coin_animation(coin_spritesheet, 14)
 
 #######
+
+################
+#for testing the map while making it
+
+for x in platforms:
+    x.x -= 2000
+for x in non_collideable_objects:
+    x.x -= 2000
+for x in horizontally_moving_platforms:
+    x.x -= 2000; x.rect.x = x.x; x.start_position_x -= 2000; 
+
 
 #pressure plate activation#
 
@@ -221,7 +294,7 @@ def pressure_plate_and_lever_effects(): #the things they activate
         if (wall_under_platform_2.start_position_y - wall_under_platform_2.y) > 0:
             wall_under_platform_2.y += 5; wall_under_platform_2.rect.y = wall_under_platform_2.y
 
-    if lever_1.on != True and lever_2.on != True and lever_3.on == True:
+    if (lever_1.on != True and lever_2.on != True and lever_3.on == True) or plate_2.activated == True:
         if (wall_3_above.start_position_y - wall_3_above.y) > -130:
             wall_3_above.y += 5; wall_3_above.rect.y = wall_3_above.y
     else:
@@ -234,6 +307,33 @@ def pressure_plate_and_lever_effects(): #the things they activate
     else:
         if (platform_6_extension.x - platform_6_extension.start_position_x) > 0:
             platform_6_extension.x -= 5; platform_6_extension.rect.x = platform_6_extension.x
+
+    if (lever_1.on == True and lever_2.on == True and lever_3.on == True) or lever_4.on == True:
+        if (wall_4.start_position_y - wall_4.y) < 200:
+            wall_4.y -= 5; wall_4.rect.y = wall_4.y
+    else:
+        if (wall_4.start_position_y - wall_4.y) > 0:
+            wall_4.y += 5; wall_4.rect.y = wall_4.y
+
+    if plate_3.activated == True and lever_5.on != True and lever_6.on == True:
+        if (wall_5_upper_part.y - wall_5_upper_part.start_position_y) < 200:
+            wall_5_upper_part.y += 5; wall_5_upper_part.rect.y = wall_5_upper_part.y
+    else:
+        if (wall_5_upper_part.y - wall_5_upper_part.start_position_y) > 0:
+            wall_5_upper_part.y -= 5; wall_5_upper_part.rect.y = wall_5_upper_part.y
+
+    if lever_5.on == True and lever_6.on != True and plate_4.activated == True:
+        if (wall_5_under_part.start_position_y - wall_5_under_part.y) < 200:
+            wall_5_under_part.y -= 5; wall_5_under_part.rect.y = wall_5_under_part.y
+    else:
+        if (wall_5_under_part.start_position_y - wall_5_under_part.y) > 0:
+            wall_5_under_part.y += 5; wall_5_under_part.rect.y = wall_5_under_part.y
+
+    if plate_5.activated == True and lever_9.on == True and lever_8.on != True:
+        if (wall_7.start_position_y - wall_7.y) < 100:
+            wall_7.y -= 5; wall_7.rect.y = wall_7.y
+        if (platform_12.x - platform_12.start_position_x) < 150:
+            platform_12.x += 7.5; platform_12.rect.x = platform_12.x
 
 #camera#
 
@@ -250,11 +350,11 @@ def camera(player_1, player_2, platforms, horizontally_moving_platforms,SCREEN_W
         move = 0
 
     for platform in platforms:
-        platform.x += move; platform
+        platform.x += move
     for object in non_collideable_objects:
         object.x += move; object.rect.x = object.x
     for horizontally_moving_platform in horizontally_moving_platforms:
-        horizontally_moving_platform.start_position_x += move; horizontally_moving_platform.x += move
+        horizontally_moving_platform.start_position_x += move; horizontally_moving_platform.x += move; horizontally_moving_platform.rect.x = horizontally_moving_platform.x 
   
     player_1.x += move; player_1.rect.x = player_1.x
     player_2.x += move; player_2.rect.x = player_2.x
@@ -266,9 +366,7 @@ while game == True:
 
     screen.fill((255,255,255))
 
-    pygame.draw.rect(screen, floor.colour, floor)
-    pygame.draw.rect(screen, grass.colour, grass)
-    pygame.draw.rect(screen, roof.colour, roof.rect)
+    #screen.blit(background, (0,0))
 
         ###################################
  
@@ -291,7 +389,7 @@ while game == True:
     pygame.draw.rect(screen, wall_left.colour, wall_left)
     pygame.draw.rect(screen, platform_1.colour, platform_1)
 
-    pygame.draw.rect(screen, platform_2_plate.colour, platform_2_plate.rect)
+    pygame.draw.rect(screen, platform_2_plate.colour, platform_2_plate)
     pygame.draw.rect(screen, platform_2.colour, platform_2)
 
     pygame.draw.rect(screen, wall_under_platform_2.colour, wall_under_platform_2)
@@ -299,23 +397,70 @@ while game == True:
 
     #pygame.draw.rect(screen, (0,0,0), lever_1.rect)
 
-    pygame.draw.rect(screen, wall_3_below.colour, wall_3_below.rect)
-    pygame.draw.rect(screen, wall_3_above.colour, wall_3_above.rect)
+    pygame.draw.rect(screen, wall_3_below.colour, wall_3_below)
+    pygame.draw.rect(screen, wall_3_above.colour, wall_3_above)
 
     screen.blit(lever_1.sprite, (lever_1.x, lever_1.y))
 
-    pygame.draw.rect(screen, platform_3.colour, platform_3.rect)
+    pygame.draw.rect(screen, platform_3.colour, platform_3)
 
     screen.blit(lever_2.sprite, (lever_2.x, lever_2.y))
 
-    pygame.draw.rect(screen, platform_4.colour, platform_4.rect)
+    pygame.draw.rect(screen, platform_4.colour, platform_4)
 
-    pygame.draw.rect(screen, platform_5.colour, platform_5.rect)
+    pygame.draw.rect(screen, platform_5.colour, platform_5)
 
     screen.blit(lever_3.sprite, (lever_3.x, lever_3.y))
 
     pygame.draw.rect(screen, platform_6.colour, platform_6)
     pygame.draw.rect(screen, platform_6_extension.colour, platform_6_extension)
+
+    pygame.draw.rect(screen, plate_2.colour, plate_2)
+
+    pygame.draw.rect(screen, wall_4.colour, wall_4)
+
+    screen.blit(lever_4.sprite, (lever_4.x, lever_4.y))
+
+    pygame.draw.rect(screen, platform_7.colour, platform_7)
+
+    pygame.draw.rect(screen, platform_8.colour, platform_8)
+
+    screen.blit(lever_5.sprite, (lever_5.x, lever_5.y))
+
+    screen.blit(lever_6.sprite, (lever_6.x, lever_6.y))
+
+    pygame.draw.rect(screen, plate_3.colour, plate_3)
+
+    pygame.draw.rect(screen, platform_9.colour, platform_9)
+
+    pygame.draw.rect(screen, wall_5_upper_part.colour, wall_5_upper_part)
+    pygame.draw.rect(screen, wall_5_under_part.colour, wall_5_under_part)
+
+    pygame.draw.rect(screen, plate_4.colour, plate_4)
+
+    pygame.draw.rect(screen, wall_6.colour, wall_6)
+
+    pygame.draw.rect(screen, platform_10.colour, platform_10.rect)
+    
+    pygame.draw.rect(screen, plate_5.colour, plate_5)
+
+    pygame.draw.rect(screen, platform_11.colour, platform_11.rect)
+
+    screen.blit(lever_8.sprite, (lever_8.x, lever_8.y))
+
+    screen.blit(lever_9.sprite, (lever_9.x, lever_9.y))
+
+    pygame.draw.rect(screen, wall_7.colour, wall_7.rect)
+
+    pygame.draw.rect(screen, platform_12.colour, platform_12.rect)
+
+    screen.blit(lever_10.sprite, (lever_10.x, lever_10.y))
+
+##############################################
+    pygame.draw.rect(screen, floor.colour, floor)
+    pygame.draw.rect(screen, grass.colour, grass)
+    pygame.draw.rect(screen, roof.colour, roof.rect)
+####################################################
 
     for coin in coins:
         coin.animate(screen)
@@ -398,7 +543,7 @@ while game == True:
             #jump
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP: 
-                if player_2.on_something: player_2.jump = True;
+                if player_2.on_something: player_2.jump = True
 
 
         #setting player_1 direction to None if they are holding down e for a non toggleable lever so they cant move during that period
